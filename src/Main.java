bbb5c08935414f40c20b7df704b38ad7a648d86c
B4RSKY <putraakbarrafsanjani@gmail.com>
2025-10-24 14:28:42 +0700

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int angkaPertama = 10;
+        int angkaKedua = 20;
+
+        int hasil = angkaPertama + angkaKedua;
+
+        System.out.println("Hasil " + hasil);
+    }
+}
